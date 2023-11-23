8319bb679be5edf037f8753202a9a4d4e01b0747
Vanessa Osorio <vanessa.osorios@opitech.com.co>
2023-11-23 11:26:44 -0500

    creacion de las clases para testing

---
+++ src/test/java/RockyCode/AlbumTest.java
@@ -0,0 +1,6 @@
+package RockyCode;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AlbumTest {
+  
+}
---
+++ src/test/java/RockyCode/BandaTest.java
@@ -0,0 +1,7 @@
+package RockyCode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BandaTest {
+
+}
